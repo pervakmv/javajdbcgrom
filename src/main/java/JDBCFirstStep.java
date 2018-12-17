db41de96e2360314f0f2dd66a7833f903d44135d
Nikolay <pervakmv@elas.kiev.ua>
2018-12-17 11:27:54 +0200

    First lesson for JDBCFirstStep

---
+++ src/main/java/JDBCFirstStep.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+public class JDBCFirstStep {
+
+    private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_URL = "jdbc:oracle:thin:@pervakmv-gromcode-lessons.ckeuefrwg72q.us-east-2.rds.amazonaws.com:1521:ORCL";
+
+    private static String USER = "main";
+    private static String PASS = "FastovFox2278";
+
+    public static void main(String[] args) {
+
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+
+
+            //1. DB Driver
+            //2. create connection открытие этого самого моста, который обеспечен и проложен нашим драйвером
+            //3. create query
+            //4. execute query
+            //5. work with result
+            //6. close all the connection
+            try { //регистрация драйвера
+                Class.forName(JDBC_DRIVER);
+            } catch (ClassNotFoundException e) {
+                return;
+            }
+
+
+            try (ResultSet resultSet = statement.executeQuery("SELECT *FROM TEST")) {
+                System.out.println("Start");
+                ResultSetMetaData rsmd = resultSet.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                System.out.println("columnsNumber = " + columnsNumber); //правильно определяет количество колонок
+             //   System.out.println(resultSet.getString(1));
+
+                while (resultSet.next()) {
+                    //TODO do something
+                    System.out.println("Object found");
+                }
+            }
+        }  catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+}
+
