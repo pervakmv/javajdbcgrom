45df9755c131a13e14b040b7776a1290828d6d82
Nikolay <pervakmv@elas.kiev.ua>
2019-02-10 23:20:21 +0200

    Lesson4.HW

--- src/main/java/hibernate/lesson2/HW/ProductDAO.java
+++ src/main/java/hibernate/lesson2/HW/ProductDAO.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.*;
 import org.hibernate.query.Query;
 
 import java.util.ArrayList;
